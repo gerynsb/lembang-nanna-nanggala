3d99396529b85a416eb8ca55b95e389c8045ce2d
gerynsb <deanpasamba02@gmail.com>
2025-07-24 22:11:42 +0800

    feat : fix

--- src/components/SectionHubungi.tsx
+++ src/components/SectionHubungi.tsx
@@ -17,7 +17,7 @@ export default function SectionHubungi() {
         <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-white mb-6">
           Siap mengunjungi Desa Lembang Nanna Nanggala ?
         </h2>
-        <Link href="https://wa.me/081343774233" target="_blank">
+        <Link href="https://wa.me/+6281343774233" target="_blank">
           <button className="bg-blue-600 cursor-pointer hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded-full shadow-lg transition-all">
             HUBUNGI KAMI
           </button>
