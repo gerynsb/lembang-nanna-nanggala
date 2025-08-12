65b405aa13ae0745e2f9317291dcb554f4b8a668
gerynsb <deanpasamba02@gmail.com>
2025-08-12 11:42:42 +0800

    ganti nama

--- src/components/SectionAboutNanna.tsx
+++ src/components/SectionAboutNanna.tsx
@@ -293,7 +293,7 @@ export default function SectionAboutNanna() {
             {/* Header Section Dusun Keempat */}
             <div className="text-center mb-12">
               <h3 className="text-2xl md:text-3xl font-semibold text-blue-400 mb-8">
-                Dusun Buntu Palilli&apos;
+                Dusun Palilli'&apos;
               </h3>
             </div>
 
